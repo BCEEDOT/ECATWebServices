fcb0c2c7cd173cf9b963c14107eaf8b721cc42ed
Jason Silvers <enderjs@gmail.com>
2017-03-29 16:33:51 -0500

    --Initial Setup

---
+++ EF/Contexts/ValueContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using EF.Models;
+
+namespace EF.Contexts
+{
+    public class ValueContext : DbContext
+    {
+        private const string LocalDb = "Server=(localdb)\\mssqllocaldb;Database=ecatlocaldev;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public static string ContextName { get { return "ValueContext"; } }
+
+        public ValueContext() : base(LocalDb) { }
+
+        public ValueContext(string connectionString = LocalDb) : base(connectionString)
+        { 
+
+        }
+
+
+        public DbSet<ValueItem> ValueItems { get; set; }
+
+
+    }
+}
